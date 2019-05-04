6458b0f937e8270e5a937fafec95d014cfd1b0aa
GitPhy <50228039+GitPhy@users.noreply.github.com>
2019-05-04 10:10:27 +0800

    Download .jpg picture from internet

---
+++ DownloadJpg.py
@@ -0,0 +1,13 @@
+# import urllib.request
+# import subprocess
+# print('Beginning file download with urllib.request')
+# JpgUrl = 'https://ucarecdn.com/85b5644f-e692-4855-9db0-8c5a83096e25/filename.jpg'
+# urllib.request.urlretrieve(JpgUrl, 'Cat2.jpg')
+# print('Finished file download with urllib.request')
+# subprocess.call('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\\Firefox.exe')
+# print(os.getcwd())
+
+import os
+os.system('notepad')
+print(ad)
+
