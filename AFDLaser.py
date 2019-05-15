35624e07ecf2aad5893e0bfc31d30b3a18612f75
GitPhy <50228039+GitPhy@users.noreply.github.com>
2019-05-16 00:41:38 +0800

    Update AFDLaser.py

--- AFDLaser.py
+++ AFDLaser.py
@@ -4,18 +4,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-NumFreq = 1001;
+NumFreq = 81;
 Freq = np.linspace(-10e9,10e9,NumFreq)
-R1 =  np.exp(-(Freq-3.2e9)**2/12e18)
-R2 = np.exp(-(Freq+3.2e9)**2/12e18)
+Sigma = 4e9
+R1 =  np.exp(-(Freq-3.2e9)**2/Sigma**2)
+R2 = np.exp(-(Freq+3.2e9)**2/Sigma**2)
 # R2 = 1- np.exp(-(Freq+8e9)**2/10e18)-np.exp(-(Freq-8e9)**2/10e18)
 
+FreqShow = Freq*1e-9
+
 plt.subplot(1,2,1)
-plt.plot(Freq,R1)
-plt.plot(Freq,R2)
+plt.plot(FreqShow,R1)
+plt.plot(FreqShow,R2)
+plt.legend(['R1','R2'])
+plt.xlabel('Frequency (GHz)')
+plt.ylabel('Ratio')
 
 plt.subplot(1,2,2)
-plt.plot(R1,R2)
-plt.axis('tight')
+plt.plot(R1,R2,'o')
+plt.xlabel('R1')
+plt.ylabel('R2')
+
 plt.show()
 
